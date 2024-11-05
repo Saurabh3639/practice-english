595106130cab47ddc5eb393b956b5eb935fe45b8
Saurabh Phawade <saurvphawade09@gmail.com>
2024-11-05 23:30:57 +0530

    Updated code in practice-english category and game pages, added new game data, and modified sidebar component

--- app/practice-english/[category]/[game]/page.js
+++ app/practice-english/[category]/[game]/page.js
@@ -1,9 +1,33 @@
-import React from 'react'
+"use client";
 
-export default async function GamePage({ params }) {
-    const { game } = await params;
+import { usePathname, useRouter } from "next/navigation";
+import React, { use } from "react";
+import { IoArrowBack } from "react-icons/io5";
+
+export default function GamePage({ params }) {
+  const router = useRouter();
+  const pathname = usePathname();
+  const basePath = pathname.split("/").slice(0, 2).join("/");
+
+  const unwrappedParams = use(params); // Use the `use` hook to unwrap `params`
+  const { category, game } = unwrappedParams; // Access the properties from the unwrapped `params`
 
   return (
-    <div>GamePage: {game}</div>
-  )
+    <div className="px-6 py-4">
+      {/* block 1 */}
+      <div className="flex justify-between items-center mb-4">
+        <div className="flex items-center gap-4 mb-4">
+          <IoArrowBack
+            className="cursor-pointer text-2xl"
+            onClick={() => router.push(`${basePath}/${category}`)}
+          />
+          <span className="text-primary text-xl font-medium">
+            {game.replace(/-/g, " ")}
+          </span>
+        </div>
+      </div>
+
+      <div>GamePage: {game}</div>
+    </div>
+  );
 }
--- app/practice-english/[category]/page.js
+++ app/practice-english/[category]/page.js
@@ -2,20 +2,85 @@ import React from "react";
 import PracticeCategoryPage from "../_components/PracticeCategoryPage";
 
 const categoriesData = {
+  // vocabulary: [
+  //   { id: 1, name: "Daily Vocab", gamesData: [{ id: 1, name: "Daily Vocab" }] },
+  //   {
+  //     id: 2,
+  //     name: "Synonym and Antonym",
+  //     gamesData: [{ id: 1, name: "Synonym and Antonym" }],
+  //   },
+  //   {
+  //     id: 3,
+  //     name: "Choose the Correct Word",
+  //     gamesData: [{ id: 1, name: "Choose the Correct Word" }],
+  //   },
+  //   {
+  //     id: 4,
+  //     name: "Scrambled Word",
+  //     gamesData: [{ id: 1, name: "Scrambled Word" }],
+  //   },
+  // ],
+  grammar: [
+    {
+      id: 1,
+      name: "Verb Tenses",
+      gamesData: [
+        { id: 1, name: "Rearrage in correct verbe tense" },
+        { id: 2, name: "Choose the correct verb tense" },
+        { id: 3, name: "Choose the correct form of verb" },
+      ],
+    },
+    {
+      id: 2,
+      name: "Nouns",
+      gamesData: [
+        { id: 1, name: "Choose the correct noun" },
+        { id: 2, name: "Types of nouns" },
+        { id: 3, name: "Noun Puzzle" },
+      ],
+    },
+    {
+      id: 3,
+      name: "Pronouns",
+      gamesData: [
+        { id: 1, name: "Fill in the blank with Reflexive Pronoun" },
+        { id: 2, name: "Fill in the blank with Pronoun Case" },
+        { id: 3, name: "Fill in the blank with Relative Pronoun" },
+      ],
+    },
+    {
+      id: 4,
+      name: "Preposition",
+      gamesData: [{ id: 1, name: "Choose the correct Preposition" }],
+    },
+    {
+      id: 5,
+      name: "Adjectives & Adverbs",
+      gamesData: [
+        { id: 1, name: "Choose the correct Adjective" },
+        { id: 2, name: "Choose the correct Adverb" },
+      ],
+    },
+    {
+      id: 6,
+      name: "Sentence Types",
+      gamesData: [{ id: 1, name: "Determine the type of each sentence" }],
+    },
+  ],
   vocabulary: [
     { id: 1, name: "Daily Vocab" },
     { id: 2, name: "Synonym and Antonym" },
     { id: 3, name: "Choose the Correct Word" },
     { id: 4, name: "Scrambled Word" },
   ],
-  grammar: [
-    { id: 1, name: "Verb Tenses" },
-    { id: 2, name: "Nouns" },
-    { id: 3, name: "Pronouns" },
-    { id: 4, name: "Preposition" },
-    { id: 5, name: "Adjectives & Adverbs" },
-    { id: 6, name: "Sentence Types" },
-  ],
+  // grammar: [
+  //   { id: 1, name: "Verb Tenses" },
+  //   { id: 2, name: "Nouns" },
+  //   { id: 3, name: "Pronouns" },
+  //   { id: 4, name: "Preposition" },
+  //   { id: 5, name: "Adjectives & Adverbs" },
+  //   { id: 6, name: "Sentence Types" },
+  // ],
 };
 
 export default async function CategoryPage({ params }) {
