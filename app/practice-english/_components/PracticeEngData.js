aa9c6335be9641ae20d6108845a8cd95efb1c1f7
Saurabh Phawade <saurvphawade09@gmail.com>
2024-11-09 22:04:33 +0530

    Moved categoriesData from page.js to PracticeEngData.js and imported it

---
+++ app/practice-english/_components/PracticeEngData.js
@@ -0,0 +1,55 @@
+export const categoriesData = {
+  grammar: [
+    {
+      id: 1,
+      name: "Verb Tenses",
+      gamesData: [
+        { id: 1, name: "Rearrange in correct verb tense" },
+        { id: 2, name: "Choose the correct verb tense" },
+        { id: 3, name: "Choose the correct form of verb" },
+      ],
+    },
+    {
+      id: 2,
+      name: "Nouns",
+      gamesData: [
+        { id: 1, name: "Choose the correct noun" },
+        { id: 2, name: "Types of nouns" },
+        { id: 3, name: "Noun Puzzle" },
+      ],
+    },
+    {
+      id: 3,
+      name: "Pronouns",
+      gamesData: [
+        { id: 1, name: "Fill in the blank with Reflexive Pronoun" },
+        { id: 2, name: "Fill in the blank with Pronoun Case" },
+        { id: 3, name: "Fill in the blank with Relative Pronoun" },
+      ],
+    },
+    {
+      id: 4,
+      name: "Preposition",
+      gamesData: [{ id: 1, name: "Choose the correct Preposition" }],
+    },
+    {
+      id: 5,
+      name: "Adjectives & Adverbs",
+      gamesData: [
+        { id: 1, name: "Choose the correct Adjective" },
+        { id: 2, name: "Choose the correct Adverb" },
+      ],
+    },
+    {
+      id: 6,
+      name: "Sentence Types",
+      gamesData: [{ id: 1, name: "Determine the type of each sentence" }],
+    },
+  ],
+  vocabulary: [
+    { id: 1, name: "Daily Vocab" },
+    { id: 2, name: "Synonym and Antonym" },
+    { id: 3, name: "Choose the Correct Word" },
+    { id: 4, name: "Scrambled Word" },
+  ],
+};
